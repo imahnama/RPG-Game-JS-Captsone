f6409a03cb3af21631e2dd02a3987dfabc2f7d13
imahnama <muannama25@gmail.com>
2020-09-06 23:28:17 +0300

    Add webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+let SRC = path.resolve(__dirname, 'src/main/js');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +10,24 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+      test: /\.mp3$/,
+      include: SRC,
+      loader: 'file-loader'
+    },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
 };
