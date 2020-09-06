040b8b1085e0c0378f98530ce6f6a2f5351791cd
imahnama <muannama25@gmail.com>
2020-09-06 23:26:33 +0300

    Add chest class

---
+++ src/classes/Chest.js
@@ -0,0 +1,27 @@
+import Phaser from 'phaser';
+
+export default class Chest extends Phaser.Physics.Arcade.Image {
+  constructor(scene, x, y, key, frame) {
+    super(scene, x, y, key, frame);
+    this.scene = scene; //the scene this game object will be added to
+    this.coins = 10; // the amount of coins this chest contains
+
+    // enable Physics
+    this.scene.physics.world.enable(this);
+    // add the player to our existing scene
+    this.scene.add.existing(this);
+
+  }
+
+  makeActive() {
+    this.setActive(true);
+    this.setVisible(true);
+    this.body.checkCollision.none = false;
+  }
+
+  makeInactive() {
+    this.setActive(false);
+    this.setVisible(false);
+    this.body.checkCollision.none = true;
+  }
+}
