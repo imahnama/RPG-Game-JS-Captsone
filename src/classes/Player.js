c849b63230291c1b0714d6204a5548e39d161470
imahnama <muannama25@gmail.com>
2020-09-06 23:26:46 +0300

    Add player class

---
+++ src/classes/Player.js
@@ -0,0 +1,39 @@
+import Phaser from 'phaser';
+
+export default class Player extends Phaser.Physics.Arcade.Image {
+  constructor(scene, x, y, key, frame) {
+    super(scene, x, y, key, frame);
+    this.scene = scene;
+    this.velocity = 160; // the velocity when moving our player
+
+    // enable Physics
+    this.scene.physics.world.enable(this);
+    // set immovable if another object collides with our Player
+    this.setImmovable(false)
+    // scale our Player
+    this.setScale(2);
+    // collide with world bounds
+    this.setCollideWorldBounds(true);
+    // add the player to our existing scene
+    this.scene.add.existing(this);
+
+  }
+
+  update(cursors) {
+    this.body.setVelocity(0);
+
+    if (cursors.left.isDown) {
+      this.body.setVelocityX(-this.velocity);
+    } else if (cursors.right.isDown) {
+      this.body.setVelocityX(this.velocity);
+    }
+
+    if (cursors.up.isDown) {
+      this.body.setVelocityY(-this.velocity);
+    } else if (cursors.down.isDown) {
+      this.body.setVelocityY(this.velocity);
+    }
+
+  }
+
+}
