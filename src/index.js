de69cf52cd395d1c631a91adf2a5300c705c1ee2
imahnama <muannama25@gmail.com>
2020-09-08 21:27:38 +0300

    add import statements for new Scenes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import config from './Config/config';
 import GameScene from './scenes/GameScene';
 import BootScene from './scenes/BootScene';
 import TitleScene from './scenes/TitleScene';
+import GameOverScene from './scenes/GameOverScene';
 import UiScene from './scenes/UiScene';
 import PreloaderScene from './scenes/PreloaderScene';
+import CreditsScene from './scenes/CreditsScene';
+import LeaderboardScene from './scenes/LeaderboardScene';
 
 
 class Game extends Phaser.Game {
@@ -14,6 +17,9 @@ class Game extends Phaser.Game {
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('Title', TitleScene);
     this.scene.add('Game', GameScene);
+    this.scene.add('GameOver', GameOverScene);
+    this.scene.add('Credits', CreditsScene);
+    this.scene.add('Leader', LeaderboardScene);
     this.scene.add('Ui', UiScene);
     this.scene.start('Boot');
   }
