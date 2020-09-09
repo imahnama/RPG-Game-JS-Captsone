b322d070641883d0f460b359d76c07b8acbf0c78
imahnama <muannama25@gmail.com>
2020-09-09 13:40:49 +0300

    Add credits scene

--- src/scenes/CreditsScene.js
+++ src/scenes/CreditsScene.js
@@ -1,7 +1,32 @@
 import Phaser from 'phaser'
+import UiButton from '../objects/UiButton';
+import config from '../Config/config'
 
-export default class CreditScene extends Phaser.Scene {
+export default class CreditsScene extends Phaser.Scene {
   constructor() {
-    super('Credits')
+    super('Credits');
   }
+
+  create() {
+
+    this.text = this.add.text(250, 40, 'How to play😎', { fontSize: 40 });
+
+
+    this.leftText = this.add.text(250, 100, 'Move the player to the left ⬅️', { fontSize: 24 });
+
+
+    this.rightText = this.add.text(250, 160, 'Move the player to the right ➡️', { fontSize: 24 });
+
+
+    this.upText = this.add.text(250, 220, 'Move the player up ⬆️', { fontSize: 24 });
+
+    this.downText = this.add.text(250, 280, 'Move the player down ⬇️', { fontSize: 24 });
+
+    this.menuButton = new UiButton(this, 400, 400, 'button1', 'button2', 'Play', this.startScene.bind(this, 'Game'));
+
+  }
+
+  startScene(targetScene) {
+   this.scene.start(targetScene);
+ }
 }
